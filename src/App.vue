690174147c2e35786c56e7e08c839669697d44c1
Wilmer <wilmersolorzanoabatta@gmail.com>
2022-08-29 20:55:20 -0400

    Mi primer commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <mi-componente/>
+  
+  <img alt="Vue logo" src="./assets/logo.png">
+  
+  <hello-world/>
+  
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MiComponente from "@/components/MiComponente.vue";
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MiComponente
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>Mi primer componente en Vue</p>
+</template>
+
+<script>
+  export default {
+    name: "MiComponente"
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
